47ddcf410bbe814dd7ed66a82baabbab7c4e78e4
João Macaoli <devjoaom@gmail.com>
2024-09-05 05:57:08 -0300

    set access control

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './guards/auth.guards';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +19,11 @@ export class AuthController {
   @Post('login')
   login(@Body() input: { email: string; password: string }) {
     return this.authService.authenticate(input);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('me')
+  getUserInfo(@Request() request) {
+    return request.user;
   }
 }
