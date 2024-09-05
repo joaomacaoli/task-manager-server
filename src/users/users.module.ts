e479acf1584f4818612911b5b467fdc890a54fa5
João Macaoli <devjoaom@gmail.com>
2024-09-05 07:04:13 -0300

    set authentication in all users crud methods

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { JwtAuthModule } from 'src/jwt/jwt.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), JwtAuthModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
